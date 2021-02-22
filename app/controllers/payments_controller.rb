59cabf34b0744882cb06647e59ac5af0db9c4164
Usman Ghani <usman.ghani720@gmail.com>
2021-02-23 01:02:23 +0500

    fetch accounts api added

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,6 +6,15 @@ class PaymentsController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    if current_user.stripe_user_id.nil?
+      @connected_accounts = Stripe::Account.list().data 
+      if @connected_accounts.present?
+        @index = @connected_accounts.pluck(:email).find_index(current_user.email)
+        if @index.present?
+          current_user.update(stripe_user_id: @connected_accounts[@index].id)
+        end
+      end
+    end
   end
 
   def create
